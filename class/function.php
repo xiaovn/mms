c3ccaaf95ef6cd1e38ff5be2a3b3706fbf61d9d0
kenzaki <xiaoproject1475>
2013-07-08 17:04:39 +0700

    Update AutoCreateID Function 17:05 PM - 08-07-2013

---
+++ class/function.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Project: mms.
+ * File name: function.php
+ * Author: sangtd
+ * Date: 7/8/13
+ * Time: 4:57 PM
+ * Email: kenzaki@xiao.vn
+ */
+include_once "Database.Class.php";
+include_once "AccountMMS.Class.php";
+include_once "Contact.Class.php";
+include_once "Info.Class.php";
+include_once "Member.Class.php";
+include_once "MetaMMS.Class.php";
