63d1452ed610cc3136df02c129bd75d37f48e9d6
kenzaki <xiaoproject1475>
2013-07-08 13:17:20 +0700

    Update Class Contact - Fix namespace - 01:16 PM - 08/07/2013.

--- class/Contact.Class.php
+++ class/Contact.Class.php
@@ -12,5 +12,21 @@ namespace mms;
 
 
 class ContactClass {
-
+  public function AddContact($uid,$metaid,$meta)
+  {
+    if(mysql_num_rows(mysql_query("SELECT * FROM `contact` WHERE id='{$uid}' and metaid='{$metaid}'")) > 0)
+    {
+        mysql_query("UPDATE `contact` SET `meta` = '{$meta}' WHERE id='{$uid}' and metaid='{$metaid}' LIMIT 1 ;");
+    }
+    else
+    {
+        @$a=mysql_query("INSERT INTO contact(id,metaid,meta) VALUES ('{$uid}', '{$metaid}','{$meta}')");
+    }
+  }
+  public function GetContact($uid,$metaid)
+  {
+    $ctl = @mysql_query("SELECT * FROM `contact` WHERE id='{$uid}' and $metaid = '{$metaid}'");
+    $ct = @mysql_fetch_array( $ctl );
+    return $ct['meta'];
+  }
 }
