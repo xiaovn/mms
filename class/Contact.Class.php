60e452b50e6306beee99a7acf71fc5d52481f07f
kenzaki <xiaoproject1475>
2013-07-08 11:42:21 +0700

    Update các function về info và demo test root nhé! lần 2, fix lỗi hiển thị

---
+++ class/Contact.Class.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Project: mms.
+ * File name: Contact.Class.php
+ * Author: sangtd
+ * Date: 7/8/13
+ * Time: 10:42 AM
+ * Email: kenzaki@xiao.vn
+ */
+
+namespace mms;
+
+
+class ContactClass {
+
+}
--- test.php
+++ test.php
@@ -8,7 +8,12 @@
  * Email: kenzaki@xiao.vn
  */
 include_once("./class/Database.Class.php");
+include_once("./class/Info.Class.php");
 $connect = new \mms\Database();
 $info = @mysql_query("SELECT * FROM `info` WHERE id='312157777'");
 $ui = @mysql_fetch_array( $info );
 echo $ui['fullname'];
+$inf = new \mms\InfoClass();
+$inf->SetPhone("312157777","0935765797");
+$email = $inf->GetInfo("email","312157777");
+echo $email;
